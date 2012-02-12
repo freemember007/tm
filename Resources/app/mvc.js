a0464db836a218dee31919f4781eb6f6b4b489f7
Mark Lee <mark.geek.lee@gmail.com>
2012-02-12 18:01:55 +0800

    refactor

--- Resources/app/mvc.js
+++ Resources/app/mvc.js
@@ -8,20 +8,20 @@ mvc.prop = {
 	}
 }
 
-require("app/view/startup");
-require("app/view/login");
+require("app/view/startup/startup");
+require("app/view/login/login");
 
-require("app/view/_main_top");
-require("app/view/_main_bottom");
-require("app/view/_month_list");
-require("app/view/_settings");
-require("app/view/image_view");
-require("app/view/_blog_list");
-require("app/view/_photo_list");
+require("app/view/common/_main_top");
+require("app/view/common/_main_bottom");
+require("app/view/common/_month_list");
+require("app/view/common/_settings");
+require("app/view/common/image_view");
+require("app/view/main_list/_blog_list");
+require("app/view/photo_list/_photo_list");
 
-require("app/view/publish_photo");
-require("app/view/publish_blog");
-require("app/view/main_list");
+require("app/view/publish/publish_photo");
+require("app/view/publish/publish_blog");
+require("app/view/main_list/main_list");
 
 require("app/controller/login");
 require("app/controller/startup");
